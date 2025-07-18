cbea094add540dfad917fb52283ddd2b7e13bf30
deepanshu verma <deepanshuverma186@gmail.com>
2025-07-18 14:18:27 +0530

    created socket connection instance in client

--- webRTC/client/vite-project/src/components/Receiver.tsx
+++ webRTC/client/vite-project/src/components/Receiver.tsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const Receiver = () => {
+      useEffect(()=>{
+            const socket = new WebSocket('ws://localhost:8080');
+            socket.onopen =()=>{
+                socket.send(JSON.stringify({type:'receiver'}))
+            }
+        },[])
   return (
     <div>Receiver</div>
   )
