2969a94e0a7587cedbbb1aba9b7e701e69a2c56c
deepanshu verma <deepanshuverma186@gmail.com>
2025-07-18 14:42:50 +0530

    Now receiver receive the offer and createAnswer & send back to sender

--- webRTC/client/vite-project/src/components/Receiver.tsx
+++ webRTC/client/vite-project/src/components/Receiver.tsx
@@ -6,7 +6,20 @@ const Receiver = () => {
             socket.onopen =()=>{
                 socket.send(JSON.stringify({type:'receiver'}))
             }
+            socket.onmessage =async(event)=>{
+               const message = JSON.parse(event.data)
+               if(message.type === 'createOffer'){
+                 //create an answer 
+                 const pc = new RTCPeerConnection()
+                 pc.setRemoteDescription(message.sdp);
+                 const answer = await pc.createAnswer();
+                 await pc.setLocalDescription(answer);
+                 socket.send(JSON.stringify({type:"createAnswer",sdp:pc.localDescription}))
+               }
+            }
         },[])
+
+        
   return (
     <div>Receiver</div>
   )
