6868851586fd8a32f51b8b8e227f83f6b8e1b070
deepanshu verma <deepanshuverma186@gmail.com>
2025-07-18 14:30:11 +0530

    created sendingVideofunction in which we createoffers & SDP for receiver

--- webRTC/client/vite-project/src/App.tsx
+++ webRTC/client/vite-project/src/App.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import './App.css'
 import { Route, BrowserRouter, Routes } from 'react-router-dom'
 import  Sender  from './components/Sender'
--- webRTC/client/vite-project/src/components/Sender.tsx
+++ webRTC/client/vite-project/src/components/Sender.tsx
@@ -1,14 +1,24 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Sender = () => {
+    const [socket, setSocket] = useState<WebSocket | null>(null)
     useEffect(()=>{
         const socket = new WebSocket('ws://localhost:8080');
         socket.onopen =()=>{
             socket.send(JSON.stringify({type:'sender'}))
         }
     },[])
+   async function startSendingVideo(){
+       const pc = new RTCPeerConnection();
+       // create offer 
+       const offer = await pc.createOffer();  //sdp
+       await pc.setLocalDescription(offer);
+       socket?.send(JSON.stringify({type:'createOffer',sdp:pc.localDescription}))
+    }
   return (
-    <div>Sender</div>
+    <div>Sender
+        <button onClick={startSendingVideo} >send</button>
+    </div>
   )
 }
 
