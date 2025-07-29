45a04ec06a211859af2a6aa1070548e49e68e7b9
deepanshu verma <deepanshuverma186@gmail.com>
2025-07-29 13:34:05 +0530

    spy in testing

--- viTest/src/test/index.test.ts
+++ viTest/src/test/index.test.ts
@@ -2,6 +2,7 @@ import { expect, test, it, describe, vi } from 'vitest'
 import { app } from '..'
 import request from 'supertest'
 import { prismaClient } from '../__mocks__/db'
+import { a } from 'vitest/dist/chunks/suite.d.FvehnV49'
 
 vi.mock('../db')
 
@@ -9,15 +10,24 @@ describe('Tests the sum',()=>{
   it('sum of a and b should be 5',async()=>{
     prismaClient.sum.create.mockResolvedValue({
       id:1,
-      result:3,
+      result:5,
       a:3,
       b:2
     })
+    vi.spyOn(prismaClient.sum,"create")
     const res = await request(app).post('/sum').send({
       a:3,
       b:2
     })
+    expect(prismaClient.sum.create).toHaveBeenCalledWith({
+      data:{
+        a:3,
+        b:2,
+        result:5,
+      }
+    })
     expect(res.body.answer).toBe(5)
+    expect(res.body.id).toBe(1)
     expect(res.statusCode).toBe(200)
 
   })
