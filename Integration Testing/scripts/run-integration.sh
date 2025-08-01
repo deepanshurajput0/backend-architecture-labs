ee927e1e5620e7648cf069e8e7e8de8a60c57931
deepanshu verma <deepanshuverma186@gmail.com>
2025-08-01 15:57:38 +0530

    Added scripts of chmo +x for executable run-integration

--- Integration Testing/scripts/run-integration.sh
+++ Integration Testing/scripts/run-integration.sh
@@ -1,3 +1,4 @@
+chmod +x 
 docker compose up -d
 echo '🟡 - Waiting for database to be ready...'
 ./wait-for-it.sh "postgresql://postgres:mysecretpassword@localhost:5432/postgres" -- echo '🟢 - Database is ready!'
