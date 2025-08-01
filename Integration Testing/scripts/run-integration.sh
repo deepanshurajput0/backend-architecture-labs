01365f678cb510317c4db2e10cb96bf0c7110825
deepanshu verma <deepanshuverma186@gmail.com>
2025-08-01 16:22:19 +0530

    Add cd Integration Testing script in run-integration.sh

--- Integration Testing/scripts/run-integration.sh
+++ Integration Testing/scripts/run-integration.sh
@@ -5,7 +5,7 @@ docker compose up -d
 echo '🟡 - Waiting for database to be ready...'
 ./scripts/wait-for-it.sh "postgresql://postgres:mysecretpassword@localhost:5455/postgres" -- echo '🟢 - Database is ready!'
 
-cd ..
+cd "Integration Testing"
 
 npx prisma migrate dev --name init4
 npm run test
